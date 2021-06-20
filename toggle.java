33f48802f98008716d8f95fa74e7b2d868aad35e
Ankithreddy9492 <85325699+Ankithreddy9492@users.noreply.github.com>
2021-06-20 18:14:19 +0530

    Add files via upload

---
+++ toggle.java
@@ -0,0 +1,38 @@
+public class toggle
+{
+ 
+
+static void toggleChars(char str[])
+{
+
+    for (int i=0; i<str.length; i++)
+
+    {
+
+        if (str[i]>='A' && str[i]<='Z')
+
+            str[i] = (char) (str[i] + 'a' - 'A');
+
+        else if (str[i]>='a' && str[i]<='z')
+
+            str[i] = (char) (str[i] + 'A' - 'a');
+
+    }
+}
+ 
+// Driver code 
+
+public static void main(String[] args) 
+{
+
+    char str[] = "ProGraMMer".toCharArray();
+
+    toggleChars(str);
+
+    System.out.println("String after toggle ");
+
+    System.out.println(String.valueOf(str));
+}
+}
+ 
+// This code is contributed by 29AjayKumar
